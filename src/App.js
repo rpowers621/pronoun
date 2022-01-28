265e83711e34b9c3e4cc0d94ace89f1c10cc6a76
Rachel Powers <rae.powers621@gmail.com>
2022-01-28 00:02:11 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,54 +1,14 @@
-import { useState } from 'react';
-import { Container, Row, Col, Button } from 'react-bootstrap';
-import Form from 'react-bootstrap/Form';
-import './App.css';
-import DocViewer from 'react-doc-viewer';
+import FileUpload from './FileUpload';
 
 
 function App() {
-  const null_doc = [{ uri: require('') }];
-  const [selectedFile, setSelectedFile] = useState(null);
-  const [docPath, setDocPath] = useState(null_doc);
-
-  function sendFile() {
-    var file = String(selectedFile.name);
-    console.log(file);
-    fetch('/readData', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ "file": file }),
-    }).then((response) => response.json()).then((resData) => {
-      console.log(resData);
-      const docs = [{ uri: require(resData) }];
-      setDocPath(docs);
-    });
-
-  }
-
   return (
 
     <div className="App">
       <header className="App-header">
-        <h1>Pronoun</h1>
+        <h1>ProNoun</h1>
       </header>
-
-      <Container className='text-box'>
-        <Row>
-          <Col>
-            <Form>
-              <Form.Group className="mb-3" >
-                <Form.Label>Please select file to <i>Pronoun</i></Form.Label>
-                <br />
-                <input type='file' onChange={(e) => setSelectedFile(e.target.files[0])} accept='.doc,.docx,.txt' />
-              </Form.Group>
-              <Button variant="primary" type="submit" onClick={sendFile}>Submit</Button>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
-      <DocViewer documents={docPath} />
+      <FileUpload />
     </div>
 
   );
